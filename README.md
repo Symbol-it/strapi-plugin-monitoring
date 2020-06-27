f85728eda97251e3a84a8ccde1d0f79718d5d60c
Tony Deplanque <tony.deplanque@symbol-it.fr>
2020-06-27 10:27:20 +0200

    :tada: init strapi plugin

---
+++ README.md
@@ -0,0 +1,3 @@
+# Strapi plugin monitoring
+
+A quick description of monitoring.
---
+++ controllers/monitoring.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * monitoring.js controller
+ *
+ * @description: A set of functions called "actions" of the `monitoring` plugin.
+ */
+
+module.exports = {
+
+  /**
+   * Default action.
+   *
+   * @return {Object}
+   */
+
+  index: async (ctx) => {
+    // Add your own logic here.
+
+    // Send 200 `ok`
+    ctx.send({
+      message: 'ok'
+    });
+  }
+};
